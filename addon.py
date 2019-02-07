667cdbe6894392f5241ee498b1e1a3e548c70879
Jose Vergara <joss1091@gmail.com>
2019-02-07 15:31:56 -0500

    request module

--- addon.py
+++ addon.py
@@ -1,11 +1,14 @@
 import xbmcaddon
 import xbmcgui
- 
+import requests
+
 addon       = xbmcaddon.Addon()
 addonname   = addon.getAddonInfo('name')
- 
+
 line1 = "Hello World!"
 line2 = "We can write anything we want here"
 line3 = "Using Python"
- 
+url = "https://jsonplaceholder.typicode.com/todos/1"
+r = requests.get(url)
+r.json()
 xbmcgui.Dialog().ok(addonname, line1, line2, line3)
