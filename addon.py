1394939c0cac1f8f22f304ee4c66de7b543626ea
Jose Vergara <joss1091@gmail.com>
2019-02-13 18:29:21 -0500

    se agrega url prod

--- addon.py
+++ addon.py
@@ -7,7 +7,7 @@ import fcntl, socket, struct
 addon       = xbmcaddon.Addon()
 addonname   = addon.getAddonInfo('name')
 mac_address = getHwAddr('eth0')
-url = "http://192.168.50.119:8000/iptv/generate_alias?mac_address={}".format(mac_address)
+url = "http://honey-kong-www.j0cpeqen0.at.d2c.io/iptv/generate_alias?mac_address={}".format(mac_address)
 
 def getHwAddr(ifname):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -17,4 +17,4 @@ def getHwAddr(ifname):
 
 
 response = requests.get(url).json()
-xbmcgui.Dialog().ok(addonname, response['message'], '',response['number'])
+xbmcgui.Dialog().ok(addonname, response['message'], '',"CODIGO:" +response['number'])
