abab6297ced5c71186f9e6b2133a99415499adae
Jose Vergara <joss1091@gmail.com>
2019-02-13 18:43:10 -0500

    try catch si no se puede parsear json

--- addon.py
+++ addon.py
@@ -6,8 +6,7 @@ import fcntl, socket, struct
 
 addon       = xbmcaddon.Addon()
 addonname   = addon.getAddonInfo('name')
-mac_address = getHwAddr('eth0')
-url = "http://honey-kong-www.j0cpeqen0.at.d2c.io/iptv/generate_alias?mac_address={}".format(mac_address)
+
 
 def getHwAddr(ifname):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -15,6 +14,10 @@ def getHwAddr(ifname):
     return ':'.join(['%02x' % ord(char) for char in info[18:24]])
 
 
-
-response = requests.get(url).json()
-xbmcgui.Dialog().ok(addonname, response['message'], '',"CODIGO:" +response['number'])
+mac_address = getHwAddr('eth0')
+url = "http://honey-kong-www.j0cpeqen0.at.d2c.io/iptv/generate_alias_code?mac_address={}".format(mac_address)
+try:
+    response = requests.get(url).json()
+    xbmcgui.Dialog().ok(addonname, response['message'], '',"CODIGO: " + response['number'])
+except:
+    xbmcgui.Dialog().ok("Error", "Ocurrio un error al generar el codigo, intenta mas tarde.")
